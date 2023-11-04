3549bd965530fe5cf40269d694b2706dc1accb24
rivo <149569050+rivo1123@users.noreply.github.com>
2023-11-04 16:53:08 +0200

    Add functions that play 5 rounds and keep score

--- script.js
+++ script.js
@@ -7,18 +7,49 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
   playerSelection = playerSelection.toLowerCase();
   if (playerSelection === computerSelection) {
-    console.log("Draw");
+    return "Draw";
   } else if (playerSelection === 'rock' && computerSelection === 'paper') {
-    console.log("You lost. Paper beats Rock");
+    return "You lost. Paper beats Rock";
   } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-    console.log("You won. Rock beats Scissors");
+    return "You won. Rock beats Scissors";
   } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-    console.log("You won. Paper beats Rock");
+    return "You won. Paper beats Rock";
   } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-    console.log("You lost. Scissors beat Paper");
+    return "You lost. Scissors beat Paper";
   } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-    console.log("You won. Scissors beat Paper");
+    return "You won. Scissors beat Paper";
   } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-    console.log("You lost. Rock beats Scissors");
+    return "You lost. Rock beats Scissors";
+  }
+}
+
+// Plays 5 rounds of the game and incremetns one of the score each round
+function game() {
+  let playerScore = 0;
+  let computerScore = 0;
+
+  for (let i =0; i < 5; i++) {
+    const playersChoice = prompt("Enter your choice");
+    const roundResult = playRound(playersChoice, getComputerChoice());
+    console.log(roundResult);
+
+    if (roundResult.includes("won")) {
+      playerScore++;
+    } else if(roundResult.includes("lost")) {
+      computerScore++;
+    }
+  }
+  compareScores(playerScore, computerScore);
+}
+
+//Compare player and computer scores and declare the winner
+function compareScores(playerScore, computerScore) {
+  console.log(`The score is ${playerScore}:${computerScore}`)
+  if (playerScore === computerScore) {
+    console.log("It's a draw!");
+  } else if (playerScore > computerScore) {
+    console.log("Player won!");
+  } else {
+    console.log("Computer won!");
   }
 }
